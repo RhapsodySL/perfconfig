563d7b932ba21296c543cf1536c5f9f4dbe185a2
Joseph H <rhapsodysmail@gmail.com>
2014-11-22 22:38:52 -0500

    Update update.bat

--- update.bat
+++ update.bat
@@ -1,2 +1,2 @@
-wget https://raw.githubusercontent.com/RhapsodySL/perfconfig/master/tf/cfg/autoexec.cfg -O C:\Program Files (x86)\Steam\SteamApps\common\Team Fortress 2\tf2\cfg\autoexec.cfg --no-check-certificate
+wget https://raw.githubusercontent.com/RhapsodySL/perfconfig/master/tf/cfg/autoexec.cfg -O "C:\Program Files (x86)\Steam\SteamApps\common\Team Fortress 2\tf2\cfg\autoexec.cfg" --no-check-certificate
 exit
