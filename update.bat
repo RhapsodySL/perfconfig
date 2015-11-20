da7da5469b5dbbfbf0f213780d2f5b3b0731643f
Joseph H <rhapsodysmail@gmail.com>
2015-11-19 19:39:47 -0500

    Fixed auto-updater, added 32-bit support
    Whoops. Turns out that grep really, really cares about case sensitivity; "steamapps" was spelled as "SteamApps" before, which broke it entirely and I didn't even know.
    
    On another note, I decided to use this opportunity to make the script automatically detect whether you're running a 64-bit operating system or a 32-bit operating system. It does this by checking if "Program Files (x86)" exists. If you, for some stupid reason, are running a 32-bit OS and have a folder called that in the root of your C: drive, then it'll mess up and install there instead. So good job. To fix that, just get rid of that folder.
    
    If anyone experiences issues with this, then tell me. For reference, though, this iteration of the script doesn't quite support custom installation paths. So to make it easier, I've added an updater-legacy.bat. If you have Team Fortress 2 installed somewhere other than the default location, use that script and replace the path in it with your installation path, in the same format as the path already in that file.

--- update.bat
+++ update.bat
@@ -1,2 +1,21 @@
-wget https://raw.githubusercontent.com/RhapsodySL/perfconfig/master/tf/cfg/autoexec.cfg -O "C:\Program Files (x86)\Steam\SteamApps\common\Team Fortress 2\tf2\cfg\autoexec.cfg" --no-check-certificate
+REM | This is the auto-updater. Read the notes in the README.MD file to know how to use this properly.
+REM | This updater detects if you run a 64-bit or 32-bit operating system, and installs the config to the correct path.
+REM | If you use a path other than the default installation path, then use updater-legacy.bat instead, and replace the
+REM | path in the grep line with your installation's filepath instead. For simpleness, and so that you don't break
+REM | the script, just use updater-legacy.bat if you need to do that; it's a lot simpler.
+REM | 
+REM | Final note; I haven't actually tested the 32-bit line since I don't have a 32-bit OS. If this doesn't work properly,
+REM | then please add an issue on the github page at https://github.com/RhapsodySL/perfconfig. Thanks.
+
+:CheckOS
+IF EXIST "%PROGRAMFILES(X86)%" (GOTO 64BIT) ELSE (GOTO 32BIT)
+
+:64BIT
+wget https://raw.githubusercontent.com/RhapsodySL/perfconfig/master/tf/cfg/autoexec.cfg -O "C:\Program Files (x86)\Steam\steamapps\common\Team Fortress 2\tf\cfg\autoexec.cfg" --no-check-certificate
 exit
+
+:32BIT
+wget https://raw.githubusercontent.com/RhapsodySL/perfconfig/master/tf/cfg/autoexec.cfg -O "C:\Program Files\Steam\steamapps\common\Team Fortress 2\tf\cfg\autoexec.cfg" --no-check-certificate
+exit
+
+
