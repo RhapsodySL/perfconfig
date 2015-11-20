317f6a1eab4dcb7321d446cca451f2eb12196c58
Joseph H <rhapsodysmail@gmail.com>
2015-11-19 19:39:51 -0500

    Added legacy updater
    Use this instead of the new file if you have TF2 installed somewhere other than the default location.

---
+++ updater-legacy.bat
@@ -0,0 +1,5 @@
+REM | This is the legacy updater. Use this instead of updater.bat if you have TF2 installed somewhere other than
+REM | the default location. Replace the path below with your filepath instead, in the same format.
+
+wget https://raw.githubusercontent.com/RhapsodySL/perfconfig/master/tf/cfg/autoexec.cfg -O "C:\Program Files (x86)\Steam\SteamApps\common\Team Fortress 2\tf2\cfg\autoexec.cfg" --no-check-certificate
+EXIT
